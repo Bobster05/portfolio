bc1dd44de0f2f58d3ba1210fcf904596930fe4f6
Denis <ddbobster@gmail.com>
2022-09-21 19:44:41 +0000

    Remove old stuff

--- portfolio_django/settings.py
+++ portfolio_django/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-a!w4nf1fjzdv=v#ef!15us7+7v=1*=#rai*uta83(sp6yu$-h("
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bobster05.pythonanywhere.com']
 
 
 # Application definition
@@ -112,6 +112,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
